e5e86a18ad5d151e3f5c8c52eab4f9c10d8ad742
Anita Zarichna <100288461+AnitaZarichna@users.noreply.github.com>
2022-10-15 21:47:31 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector(`[data-start]`);
+const stopBtn = document.querySelector(`[data-stop]`);
+const body = document.querySelector(`body`);
+let timerID = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener( `click` , onStartBtn);
+stopBtn.addEventListener( `click` , onStopBtn);
+
+function onStartBtn () {
+startBtn.disabled = true;
+stopBtn.disabled = false;
+body.style.backgroundColor = getRandomHexColor ();
+timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+}, 1000);
+}
+
+function onStopBtn() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerID);
+}
