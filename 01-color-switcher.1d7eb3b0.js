c2558fab73c2e1637c78baf9c9853d96e682c564
AnitaZarichna <100288461+AnitaZarichna@users.noreply.github.com>
2022-10-17 18:01:08 +0000

    Deploying to gh-pages from @ AnitaZarichna/goit-js-hw-09@7e7701c8877f9067e0cd36e1349475f46a329bc3 🚀

---
+++ 01-color-switcher.1d7eb3b0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;function d(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,n.style.backgroundColor=d(),o=setInterval((()=>{n.style.backgroundColor=d()}),500)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.1d7eb3b0.js.map
